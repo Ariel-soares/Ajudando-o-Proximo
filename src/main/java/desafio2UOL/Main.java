94d7584306dbaddcd53d00a79460902957b86907
Ariel Soares <ariel.sfranco@gmail.com>
2024-07-05 09:04:00 -0300

    Main class used for tests, everything going OK untill now

--- src/main/java/desafio2UOL/Main.java
+++ src/main/java/desafio2UOL/Main.java
@@ -1,5 +1,9 @@
 package desafio2UOL;
 
+import desafio2UOL.entities.ClothItem;
+import desafio2UOL.entities.DistributionCenter;
+import desafio2UOL.entities.Donation;
+import desafio2UOL.entities.Item;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
@@ -10,7 +14,36 @@ public class Main {
 
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("desafio-context");
 		EntityManager em = emf.createEntityManager();
+		DistributionCenter cd = new DistributionCenter(null, "cd1", "rua 1", "campinas", "RS", "25642");
 
+		em.getTransaction().begin();
+		em.persist(cd);
+		em.getTransaction().commit();
+
+		Item cloth = new ClothItem(null, "Camisa", "Camisa Masculina branca", 'M', "M");
+		Item cloth2 = new ClothItem(null, "Moletom", "Moletom feminino preto", 'F', "M");
+
+		em.getTransaction().begin();
+		em.persist(cloth);
+		em.persist(cloth2);
+		em.getTransaction().commit();
+
+		Donation donation = new Donation(null, cd);
+
+		donation.addItem(cloth);
+		donation.addItem(cloth2);
+
+		em.getTransaction().begin();
+		em.persist(donation);
+		em.getTransaction().commit();
+
+		System.out.println("Operação concluída");
+
+		Donation result = em.find(Donation.class, 1);
+
+		for (Item item : result.getItens()) {
+			System.out.println(item);
+		}
 		emf.close();
 		em.close();
 
